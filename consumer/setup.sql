bb2a5e2d8b5f520ef2a8bc1df85a7794fd784580
Miles Adkins <miles.adkins@snowflake.com>
2023-11-16 10:04:14 -0600

    feat: first commit

---
+++ consumer/setup.sql
@@ -0,0 +1,16 @@
+-- Setup script for the Hello Snowflake! application.
+
+CREATE APPLICATION ROLE app_public;
+CREATE SCHEMA IF NOT EXISTS core;
+GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
+
+CREATE OR REPLACE PROCEDURE CORE.HELLO()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS OWNER
+  AS
+  BEGIN
+    RETURN 'Hello Snowflake!';
+  END;
+
+  GRANT USAGE ON PROCEDURE core.hello() TO APPLICATION ROLE app_public;
