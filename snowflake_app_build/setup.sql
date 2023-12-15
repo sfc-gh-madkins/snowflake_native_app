e2640ff9e5e89b76fe501db371e33f486fb597f8
Miles Adkins <miles.adkins@snowflake.com>
2023-12-15 17:04:39 -0600

    feat: Hello World
    Signed-off-by: Miles Adkins <miles.adkins@snowflake.com>

---
+++ snowflake_app_build/setup.sql
@@ -0,0 +1,37 @@
+-- Setup script for the Hello Snowflake! application.
+
+CREATE APPLICATION ROLE app_public;
+
+CREATE OR ALTER VERSIONED SCHEMA code;
+GRANT USAGE ON SCHEMA code TO APPLICATION ROLE app_public;
+
+
+CREATE OR REPLACE PROCEDURE code.hello()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS OWNER
+  AS
+  BEGIN
+    RETURN 'Hello Snowflake!';
+  END;
+  GRANT USAGE ON PROCEDURE code.hello() TO APPLICATION ROLE app_public;
+
+
+CREATE OR REPLACE FUNCTION code.addone(i int)
+RETURNS INT
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+HANDLER = 'addone_py'
+AS
+$$
+def addone_py(i):
+  return i+1
+$$;
+GRANT USAGE ON FUNCTION code.addone(int) TO APPLICATION ROLE app_public;
+
+
+CREATE STREAMLIT code.streamlit_prediction_app
+  FROM '/src/app'
+  MAIN_FILE = '/streamlit.py'
+;
+GRANT USAGE ON STREAMLIT code.streamlit_prediction_app TO APPLICATION ROLE app_public;
